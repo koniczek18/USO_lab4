66e972ed44917dcd12b44d504666bef310394713
Szymon <sz.zalas@gmail.com>
2022-11-14 19:26:20 +0100

    task 2

--- USO_Lab4.py
+++ USO_Lab4.py
@@ -18,27 +18,44 @@ def przyklad(active):
                 {'type': 'ineq', 'fun': lambda x: 1 / 2 * x[0] - x[1] - 4},
                 {'type': 'ineq', 'fun': lambda x: -12 * x[0] + 3 * x[1] + 6},
                 {'type': 'ineq', 'fun': lambda x: 2 * x[0] + x[1] + 2})
-        bnds=((None,None),(None,None))
-        result=scipy.optimize.minimize(func,(0,0),method='SLSQP',bounds=bnds,
-                                       constraints=cons)
+        bnds = ((None, None), (None, None))
+        result = scipy.optimize.minimize(func, (0, 0), method='SLSQP', bounds=bnds,
+                                         constraints=cons)
         print(result.fun)
+
+
 def zadanie1(active):
     if active:
         # objective function: -y->max
         # other variables: x
         # DEFINITION: x[0]=x, x[1]=y
-        # objective function reflecting vector: [1,0]
-        # 1st constrain: -y+2x<=4
-        # 2rd constrain: y+4x>=-2 --> -y-4x<=2
-        # 3rd constrain: x+y<3
-        # 3rd constrain changed to x+y<=2.999
-        # calculate
-        pass
+        # 1st constrain: -y+2x<=4  -> -2x+y>=-4
+        # 2rd constrain: 4x+y>=-2
+        # 3rd constrain: x+y<3 -> x+y <=2.999
+        func = lambda x: 0 * x[0] - x[1]
+        cons = ({'type': 'ineq', 'fun': lambda x: -2 * x[0] + x[1] + 4},
+                {'type': 'ineq', 'fun': lambda x: 4 * x[0] + x[1] + 2},
+                {'type': 'ineq', 'fun': lambda x: x[0] + x[1] - 2.999})
+        bnds = ((None, None), (None, None))
+        result = scipy.optimize.minimize(func, (0, 0), method='SLSQP', bounds=bnds,
+                                         constraints=cons)
+        print(result.fun)
 
 
-
+def zadanie2(active):
+    if active:
+        # objective function: x^4-4*x^3-2*x^2+12*x+9 -> min
+        # constrain x>=0
+        func = lambda x: x ^ 4 - 4 * x ^ 3 - 2 * x ^ 2 + 12 * x + 9
+        cons=({'type': 'ineq', 'fun': lambda x: x})
+        lw=[0]
+        up=np.inf
+        result=scipy.optimize.dual_annealing(func,bounds=list(zip(lw,up)))
+        print(result.fun)
+        #TODO ?????????????????????????????
 
 
 if __name__ == '__main__':
-    przyklad(True)
-    zadanie1(True)
+    przyklad(False)
+    zadanie1(False)
+    zadanie2(True)
